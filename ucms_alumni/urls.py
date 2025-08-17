e1be728bb21718a81a569e0afe29097a7ca9ef78
PRAVESH GREWAL <118418011+praveshgrewal@users.noreply.github.com>
2025-08-17 23:16:44 +0530

    Update urls.py

--- ucms_alumni/urls.py
+++ ucms_alumni/urls.py
@@ -3,19 +3,19 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve as django_static_serve
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('alumni.urls')),
-
-    # Serve user-uploaded media in production (OK for small sites)
-    re_path(r'^media/(?P<path>.*)$',
-            django_static_serve,
-            {'document_root': settings.MEDIA_ROOT}),
 ]
 
-# Optional: local dev convenience
+# Always serve user uploads from MEDIA_ROOT at /media/
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
+
+# Only needed for local dev; WhiteNoise serves static in prod
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
